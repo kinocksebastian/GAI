ef9777a04afc5fcde9aee90b352606df1f89a39f
nu11p01n73R <kjjohny1991@gmail.com>
2014-01-04 22:50:14 +0530

    modified

--- notification.py
+++ notification.py
@@ -3,9 +3,22 @@
 ##this is just a code to generate notification in gnome	. Should build from this . Just	started	with notification mechanism ..
 
 
-from gi.repository import Notify
-Notify.init ("Hello world")
-Hello=Notify.Notification.new ("SUBJECT: ","NOTIFICATION !!!","dialog-information")
-Hello.show ()
+import pynotify
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('127.0.0.1',5555))
+sock.listen(1)
+while 1:
+	conn = sock.accept()[0]
+	data = conn.recv(20)
+	pynotify.init ("Hello world")
+	if data:
+		msg = pynotify.Notification ("Message",data,"dialog-information")
+		msg.show ()
+
+
+
+
 
 
